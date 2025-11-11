3bdc7e058d881f9d3c7c97311ef0f2dab9e28d15
bourraOmar <bwrtmr@gmail.com>
2025-11-11 16:52:12 +0100

    DLD-29 Structurer les dossiers du projet.

--- daret-darna-frontend/src/features/annonces/components/AnnonceCard.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import CardAnnonce from '../../../components/shared/CardAnnonce';
-
-const AnnonceCard = ({ annonce }) => <CardAnnonce annonce={annonce} />;
-
-export default AnnonceCard;
